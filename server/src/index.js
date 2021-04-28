113953d190f7df78399dd4c61354bf0d2463b4b6
John Lee <john.lee@servian.com>
2021-04-28 23:03:54 +1000

    fix env

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const axios = require("axios");
-const redis = require("redis");
+// const redis = require("redis");
 const app = express();
 const { login, registration } = require('./routes/index');
-require('./redisClient/connection').connectToRedis();
+const redisClient = require('./redisClient/connection');
 
 app.use(express.json())
 
@@ -12,6 +12,25 @@ app.use(login);
 
 app.use(registration);
 
-app.listen(process.env.PORT || 3000, () => {
+app.get('/store/:key', async (req, res) => {
+    const { key } = req.params;
+    const value = req.query;
+    await redisClient.setAsync(key, JSON.stringify(value));
+    return res.send('Success');
+});
+
+app.get('/:key', async (req, res) => {
+    const { key } = req.params;
+    const rawData = await redisClient.getAsync(key);
+    return res.json(JSON.parse(rawData));
+});
+
+app.get('/', (req, res) => {
+    return res.send('Hello world');
+});
+
+const SERVERPORT = process.env.PORT || 3000;
+
+app.listen(SERVERPORT, () => {
     console.log("🟢 Node server started 🟢");
 });
--- server/src/redisClient/connection.js
+++ server/src/redisClient/connection.js
@@ -1,23 +1,32 @@
 const redis = require("redis");
-
-clientConnection = () => {
-    let client = redis.createClient({
-        port: 6379,
-        host: 'redis',
-        password: 'secret' // as given in docker-compose.yml file
-    });
-    console.log(`💚 You are connected to Redis Instance 💚`)
-    return client;
-}
+const {promisify} = require('util');
+const client = redis.createClient(process.env.REDIS_URL);
 
 module.exports = {
-    connectToRedis: () => {
-        try {
-            return clientConnection();
-        } catch (err) {
-                console.log(`💔 Couldn't connect to Redis 💔 ${err}`);
-                throw new Error(err);
-        }
-    },
+    ...client,
+    getAsync: promisify(client.get).bind(client),
+    setAsync: promisify(client.set).bind(client),
+    keysAsync: promisify(client.keys).bind(client)
+  };
 
-};
+// clientConnection = () => {
+//     let client = redis.createClient({
+//         port: 6379,
+//         host: 'redis',
+//         password: 'secret' // as given in docker-compose.yml file
+//     });
+//     console.log(`💚 You are connected to Redis Instance 💚`)
+//     return client;
+// }
+
+// module.exports = {
+//     connectToRedis: () => {
+//         try {
+//             return clientConnection();
+//         } catch (err) {
+//                 console.log(`💔 Couldn't connect to Redis 💔 ${err}`);
+//                 throw new Error(err);
+//         }
+//     },
+
+// };
